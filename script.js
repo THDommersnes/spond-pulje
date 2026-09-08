53ea538def7107c653268e479a9e3788dff9fced
THDommersnes <555mjk876c@privaterelay.appleid.com>
2026-09-08 11:21:36 +0200

    Create player list with dropdowns in script.js
    Replace alert with dynamic player list creation.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("TEST FRA SCRIPT.JS");
+const playerList = document.getElementById('playerList');
+
+["Spiller 1", "Spiller 2"].forEach(name => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.textContent = name + " ";
+
+  const select = document.createElement("select");
+  ["Gr1", "Gr2", "Gr3"].forEach(level => {
+    const opt = document.createElement("option");
+    opt.value = level;
+    opt.textContent = level;
+    select.appendChild(opt);
+  });
+
+  li.appendChild(span);
+  li.appendChild(select);
+  playerList.appendChild(li);
+});
