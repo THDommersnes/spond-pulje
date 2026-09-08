58e22eaeeadf99d2181a90507f1c716d3ee411bf
THDommersnes <555mjk876c@privaterelay.appleid.com>
2026-09-08 11:24:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+alert("TEST FRA SCRIPT.JS");
+
+// Finn <ul id="playerList">
 const playerList = document.getElementById('playerList');
 
+// Testnavn
 ["Spiller 1", "Spiller 2"].forEach(name => {
+
+  // Lag <li>
   const li = document.createElement("li");
+
+  // Navn
   const span = document.createElement("span");
   span.textContent = name + " ";
 
+  // Dropdown
   const select = document.createElement("select");
   ["Gr1", "Gr2", "Gr3"].forEach(level => {
     const opt = document.createElement("option");
@@ -13,7 +22,13 @@ const playerList = document.getElementById('playerList');
     select.appendChild(opt);
   });
 
+  // Sett default
+  select.value = "Gr3";
+
+  // Sett sammen
   li.appendChild(span);
   li.appendChild(select);
+
+  // Legg inn i listen
   playerList.appendChild(li);
 });
